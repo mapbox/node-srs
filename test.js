e3608c44a82d497cd24fdaa6533c93cafe1b5925
Dane Springmeyer <dane@dbsgeo.com>
2011-01-18 13:46:19 -0500

    better test coverage

--- test.js
+++ test.js
@@ -3,12 +3,46 @@
 var srs = require('srs');
 var assert = require('assert');
 //var path = require('path');
-//var fs = require('fs');
+var fs = require('fs');
 
 assert.throws(function () { srs.inspect('foo') })
-assert.equal(srs.inspect('+init=epsg:4326').proj4.trim(),'+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs');
-assert.equal(srs.inspect('+init=epsg:4326').proj4.trim(),'+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs');
+  
+var srs_input1 = '+init=epsg:4326';
+var srs1 = srs.inspect(srs_input1);
+assert.equal(srs1.proj4,'+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ');
+assert.equal(srs1.epsg,'4326');
+assert.equal(srs1.auth,'EPSG');
+assert.equal(srs1.esri,false);
+assert.equal(srs1.is_geographic,true);
+assert.equal(srs1.input,srs_input1);
 
-//console.log(srs.inspect('GEOGCS["GCS_North_American_1983_CSRS98",DATUM["D_North_American_1983_CSRS98",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]'));
+var srs_input2 = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]';
+var srs2 = srs.inspect(srs_input2);
+assert.equal(srs2.proj4,'+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ');
+assert.equal(srs2.epsg,'4326');
+assert.equal(srs2.auth,'EPSG');
+assert.equal(srs2.esri,false);
+assert.equal(srs2.is_geographic,true);
+assert.equal(srs2.input,srs_input2);
+
+var srs_input3 = 'WGS84';
+var srs3 = srs.inspect(srs_input3);
+assert.equal(srs3.proj4,'+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ');
+assert.equal(srs3.epsg,'4326');
+assert.equal(srs3.auth,'EPSG');
+assert.equal(srs3.esri,false);
+assert.equal(srs3.is_geographic,true);
+assert.equal(srs3.input,srs_input3);
+
+var data = fs.readFileSync('./data/4326.esri.prj');
+var srs_input4 = data.toString();
+var srs4 = srs.inspect(srs_input4);
+assert.equal(srs4.proj4,'+proj=longlat +ellps=WGS84 +no_defs ');
+//assert.equal(srs4.proj4,'+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ');
+assert.equal(srs4.epsg,'4326');
+assert.equal(srs4.auth,'EPSG');
+assert.equal(srs4.esri,false);
+assert.equal(srs4.is_geographic,true);
+assert.equal(srs4.input,srs_input4);
 
 console.log('All tests pass...');
