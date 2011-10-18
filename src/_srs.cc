24c7f612d77d02ad74b29a2914d4e2e09a2f57d9
Dane Springmeyer <dane@dbsgeo.com>
2011-10-17 17:44:46 -0700

    bump to 0.2.9

--- src/_srs.cc
+++ src/_srs.cc
@@ -194,7 +194,7 @@ extern "C" {
   {
 
     // node-srs version
-    target->Set(String::NewSymbol("version"), String::New("0.2.8"));
+    target->Set(String::NewSymbol("version"), String::New("0.2.9"));
 
     NODE_SET_METHOD(target, "_parse", parse);
     
