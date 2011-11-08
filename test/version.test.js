bde02263b2f618b6af65b751e2ae9bffb1a7d1b1
Dane Springmeyer <dane@dbsgeo.com>
2011-11-07 18:45:13 -0800

    find package.json one directory back

--- test/version.test.js
+++ test/version.test.js
@@ -1,5 +1,5 @@
 var srs = require('srs');
-var info = require('package.json');
+var info = require('../package.json');
 
 exports['test version updated for release'] = function(beforeExit, assert) {
     assert.equal(info.version, srs.version);
