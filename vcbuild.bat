01978174d0e13e8873e96e08fba2384d02610ad4
Dane Springmeyer <dane@dbsgeo.com>
2012-04-17 16:12:30 -0700

    clear out bundled srs_data since we'll use GDAL's on windows

--- vcbuild.bat
+++ vcbuild.bat
@@ -3,6 +3,7 @@ set GDAL_DATA=C:\dev2\gdal\data
 del build.sln
 rd /q /s Default
 del lib\\_srs.node
+rd /q /s lib\\srs_data
 python gyp/gyp build.gyp --depth=. -f msvs -G msvs_version=2010
 msbuild build.sln
 copy Default\\_srs.node lib\\_srs.node
