4328ab89393b3647ef8c22fa5a2dad4cc8fd6f41
Dane Springmeyer <dane@dbsgeo.com>
2013-01-11 15:46:24 -0800

    fix handling of +y_0=0.0 vs +y_0=0 - refs mapbox/tilemill#1866

--- src/_srs.cc
+++ src/_srs.cc
@@ -197,7 +197,7 @@ extern "C" {
   {
 
     // node-srs version
-    target->Set(String::NewSymbol("version"), String::New("0.2.18"));
+    target->Set(String::NewSymbol("version"), String::New("0.2.19"));
 
     NODE_SET_METHOD(target, "_parse", parse);
     
