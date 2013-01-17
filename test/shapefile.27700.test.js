2fb83234155fd8a291586c237b53c7f3dbbef4da
Dane Springmeyer <dane@dbsgeo.com>
2013-01-17 11:56:29 -0800

    further build out OSGB tests

--- test/shapefile.27700.test.js
+++ test/shapefile.27700.test.js
@@ -4,14 +4,27 @@ var assert = require('assert');
 
 
 describe('OSGB 1936', function() {
+
+    it('should report that it was parsed as esri if ESRI:: was manually prepended', function() {
+        var esri_srs = fs.readFileSync('./test/data/27700_esri_wkt.prj').toString();
+        var esri_result = srs.parse('ESRI::'+esri_srs);
+        assert.equal(esri_result.esri,true);
+    });
+
     // https://github.com/springmeyer/node-srs/issues/25
-    it('should detect both wkt variants the same', function() {
+    it('should detect OGC format for OSGB 1936 / British National Grid', function() {
         var ogc_srs = fs.readFileSync('./test/data/27700_ogc_wkt.prj').toString();
         var ogc = srs.parse(ogc_srs);
-        var esri_srs = fs.readFileSync('./test/data/27700_esri_wkt.prj').toString();
-        var esri = srs.parse('ESRI::'+esri_srs);
+        assert.equal(ogc.srid,27700);
+        assert.equal(ogc.name,'OSGB 1936 / British National Grid');
         assert.equal(ogc.esri,false);
-        assert.equal(ogc.name,esri.name);
-        assert.equal(esri.esri,true);
+    });
+
+    // https://github.com/springmeyer/node-srs/issues/25
+    it('should detect ESRI format for OSGB 1936 / British National Grid', function() {
+        var esri_srs = fs.readFileSync('./test/data/27700_esri_wkt.prj').toString();
+        var esri_result = srs.parse('ESRI::'+esri_srs);
+        assert.equal(esri_result.name,'OSGB 1936 / British National Grid');
+        assert.equal(esri_result.srid,27700);
     });
 });
