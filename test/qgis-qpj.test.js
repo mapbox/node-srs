cb6ce7c20bc662cac0a9ac4ec575f4f737f8b8cd
Dane Springmeyer <dane@dbsgeo.com>
2013-11-06 21:06:17 -0800

    tweak qgis qpj test

--- test/qgis-qpj.test.js
+++ test/qgis-qpj.test.js
@@ -7,10 +7,11 @@ describe('qgis qpj format', function() {
         var qgis = srs.parse(fs.readFileSync('./test/data/transverse_merc_kasey_custom_grids.prj'));
         var normal = srs.parse(fs.readFileSync('./test/data/transverse_merc_kasey.prj'));
         assert.ok(normal.proj4);
-        assert.notEqual(normal.proj4,qgis.proj4);
+        assert.ok(qgis.proj4);
         assert.ok(qgis.proj4.indexOf('nadgrids=@conus') > 0);
         assert.ok(qgis.proj4.indexOf('proj=tmerc') > 0);
         assert.ok(qgis.proj4.indexOf('ellps=clrk66') > 0);
+        assert.ok(normal.proj4.indexOf('nadgrids=@conus') == -1);
         assert.ok(normal.name,'Transverse_Mercator');
         assert.ok(normal.proj4.indexOf('proj=tmerc') > 0);
         assert.ok(normal.proj4.indexOf('ellps=clrk66') > 0);
