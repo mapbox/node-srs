7bb73a5225711e6436a72bb39a53951433fed4ff
Dane Springmeyer <dane@dbsgeo.com>
2013-11-06 17:59:17 -0800

    better vcbuild.bat"

--- vcbuild.bat
+++ vcbuild.bat
@@ -1,14 +1,7 @@
-set PROJ_LIB=C:\dev2\proj\nad
-set GDAL_DATA=C:\dev2\gdal\data
-del build.sln
-rd /q /s Default
+set PROJ_LIB=C:\mapnik-v2.3.0\share\proj
+set GDAL_DATA=C:\mapnik-v2.3.0\share\gdal
+rd /q /s build
 del lib\\_srs.node
-rd /q /s lib\\srs_data
-python gyp/gyp build.gyp --depth=. -f msvs -G msvs_version=2010
-msbuild build.sln
-copy Default\\_srs.node lib\\_srs.node
-rem test!
-echo module.exports.static_osr = false; > lib\settings.js
+npm install --nodedir=%HOMEPATH%/.node-gyp/0.10.21-mod
 set NODE_PATH=lib
-node node_modules\mocha\bin\mocha
-rem node -e "console.log(require('mapnik'))"
+npm test
