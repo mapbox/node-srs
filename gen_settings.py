cd6505fa5791f13c45e07912eb0d717e695d6a7d
Dane Springmeyer <dane@mapbox.com>
2014-04-25 18:48:42 -0700

    handle vagarities in quoting

--- gen_settings.py
+++ gen_settings.py
@@ -6,7 +6,9 @@ if __name__ == '__main__':
     settings_template = """
 module.exports.shared_gdal = %s;
     """
-    if sys.argv[1] == 'false':
+    print sys.argv[1]
+    arg = sys.argv[1].strip("'").strip('"')
+    if arg == 'false':
         open(settings,'w').write(settings_template % 'false')
     else:
         open(settings,'w').write(settings_template % 'true')
