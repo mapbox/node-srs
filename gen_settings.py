71b71935fbe37763dbdaeb1f4eba6bdfcad60c0a
Dane Springmeyer <dane@mapbox.com>
2014-04-25 18:54:14 -0700

    fix generation of settings on windows

--- gen_settings.py
+++ gen_settings.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
     settings_template = """
 module.exports.shared_gdal = %s;
     """
-    print sys.argv[1]
     arg = sys.argv[1].strip("'").strip('"')
-    if arg == 'false':
+    # windows gives ../false
+    if 'false' in arg:
         open(settings,'w').write(settings_template % 'false')
     else:
         open(settings,'w').write(settings_template % 'true')
