f500756228f2d8a33d94826f737d711029314c2f
bergwerkgis <wb@bergwerk-gis.at>
2015-08-06 13:07:47 +0200

    [publish binary] AppVeyor with npm-windows-upgrade

--- scripts/build-local.bat
+++ scripts/build-local.bat
@@ -16,10 +16,13 @@ IF EXIST "%NWU_DIR%" ECHO found %NWU_DIR%, deleting... && RD /S /Q "%NWU_DIR%"
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 
+::GOTO BUILD_MATRIX
 
 
 SET APPVEYOR_REPO_COMMIT_MESSAGE=[publish binary]
 
+
+::=============== single build ==============
 SET platform=x64
 SET nodejs_version=0.10.40
 SET msvs_toolset=14
@@ -27,10 +30,16 @@ SET TOOLSET_ARGS=--dist-url=https://s3.amazonaws.com/mapbox/node-cpp11 --toolset
 
 CALL scripts\build-appveyor.bat
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+
 GOTO DONE
 
 
+::============== build matrix
+:BUILD_MATRIX
 
+ECHO TODO^: delete npm-windows-upgrade for each run
+SET ERRORLEVEL=1 && GOTO ERROR
 
 SET TOOLSET_ARGS=
 
