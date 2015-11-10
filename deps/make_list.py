5569af3f4f3805aa1559ea6173e7a73e717c529b
Dane Springmeyer <dane@mapbox.com>
2015-11-09 17:03:34 -0800

    drop c++ tooling

--- deps/make_list.py
+++
@@ -1,8 +0,0 @@
-import glob
-import os
-
-files = []
-files.extend(glob.glob('osr/src/*.c'))
-files.extend(glob.glob('osr/src/*.cpp'))
-for f in files:
-    print "          '%s'," % f
--- vcbuild.bat
+++
@@ -1,7 +0,0 @@
-set PROJ_LIB=C:\mapnik-v2.3.0\share\proj
-set GDAL_DATA=C:\mapnik-v2.3.0\share\gdal
-rd /q /s build
-del lib\\_srs.node
-npm install --nodedir=%HOMEPATH%/.node-gyp/0.10.21-mod
-set NODE_PATH=lib
-npm test
